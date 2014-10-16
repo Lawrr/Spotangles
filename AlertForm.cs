412ca2cb5a15784a111d7fe9b23cf5b755f500af
Lawrence Ling <5894666+Lawrr@users.noreply.github.com>
2014-10-17 03:49:31 +1100

    Basic functionality working

---
+++ AlertForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spotangles {
+	public partial class AlertForm : Form {
+		public AlertForm(string[] availableClasses) {
+			InitializeComponent();
+
+			foreach (string availableClass in availableClasses) {
+				this.classBox.Items.Add(availableClass);
+			}
+		}
+	}
+}
