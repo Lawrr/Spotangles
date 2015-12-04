9729cbd2afefd2c31427af8d7f54449db85e2faf
Lawrence Ling <5894666+Lawrr@users.noreply.github.com>
2015-12-05 04:30:43 +1100

    Added Semester enum and Class class

---
+++ Class.cs
@@ -0,0 +1,17 @@
+﻿namespace Spotangles {
+    public class Class {
+
+        public string CourseCode { get; private set; }
+        public Semester Semester { get; private set; }
+
+        public string Activity { get; private set; }
+        public string Section { get; private set; }
+        public int ClassNumber { get; private set; }
+        public string Status { get; private set; }
+        public int CurrentSpots { get; private set; }
+        public int TotalSpots { get; private set; }
+
+        public Class() {
+        }
+    }
+}
