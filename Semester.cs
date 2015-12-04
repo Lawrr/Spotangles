11f1ac845e2a1aa1e54a08deaf99b6e563932efd
Lawrence Ling <Lawrence1995@hotmail.com>
2015-12-05 04:33:36 +1100

    Removed unused usings

--- Semester.cs
+++ Semester.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Spotangles {
+﻿namespace Spotangles {
     public enum Semester {
         One,
         Two,
